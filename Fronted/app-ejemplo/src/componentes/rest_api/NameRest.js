c2bbceb55107fa1f69e0ee3ff6ba843e525fb848
LuisLDA <65144234+LuisLDA@users.noreply.github.com>
2023-06-28 12:51:59 +0000

    agregado modal

--- Fronted/app-ejemplo/src/componentes/rest_api/NameRest.js
+++ Fronted/app-ejemplo/src/componentes/rest_api/NameRest.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, TextInput, TouchableOpacity, View, Modal } from "react-native";
 
 const NameRest = () => {
 
     const [nombre, setNombre] = useState('');
     const [apellido, setApellido] = useState('');
+    const [data, setData] = useState(null);
+    const [modalVisible, setModalVisible] = useState(false);
 
     const serverUrl = "https://luislda-cautious-space-waddle-xx56rvvg475c9977-3000.preview.app.github.dev";
 
@@ -12,37 +14,63 @@ const NameRest = () => {
         try {
             const response = await fetch(`${serverUrl}/hola/:${nombre}/:${apellido}`);
             const jsonData = await response.json();
-            console.log(jsonData)
+            console.log("jsonData", jsonData);
+            setData(jsonData);
         } catch (e) {
             console.error("error", e);
         }
     };
 
-    useEffect(() => {
-        fetchData();
-    }, [nombre, apellido]);
 
-    const saludar = () => {
+    const imprimirEnConsola = () => {
         fetchData();
+        setModalVisible(true);
     };
 
     return (
         <View style={styles.container}>
             <Text style={styles.titleContainer}>
-                Conexión con el servidor 
+                Conexión con el servidor
             </Text>
             <View style={styles.serverContainer}>
                 <Text style={styles.titleServer}>Ingrese los siguientes datos</Text>
                 <TextInput style={styles.inputServer} onChangeText={setNombre} value={nombre} placeholder="Ingrese su nombre" />
-                <TextInput style={styles.inputServer} onChangeText={setApellido} value={apellido} placeholder="Ingrese su apellido"/>
+                <TextInput style={styles.inputServer} onChangeText={setApellido} value={apellido} placeholder="Ingrese su apellido" />
                 <View style={styles.buttonContainer}>
-                    <TouchableOpacity style={styles.buttonServer} onPress={saludar}>
+                    <TouchableOpacity style={styles.buttonServer} onPress={imprimirEnConsola}>
                         <Text>Enviar Datos</Text>
                     </TouchableOpacity>
                 </View>
             </View>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    setModalVisible(!modalVisible);
+                }}
+            >
+                <View style={styles.modalContainer}>
+                    <View style={styles.modalContent}>
+                        <Text style={styles.modalTitle}>Información recibida</Text>
+                        {data && (
+                            <Text style={styles.modalText}>Nombre{data.nombre}</Text>
+                        )}
+
+                        {data && (
+                            <Text style={styles.modalText}>Apellido{data.apellido}</Text>
+                        )}
+                        <TouchableOpacity
+                            style={[styles.buttonServer, styles.modalButton]}
+                            onPress={() => setModalVisible(!modalVisible)}
+                        >
+                            <Text>Cerrar</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Modal>
         </View>
-    )    
+    )
 }
 
 const styles = StyleSheet.create({
@@ -79,6 +107,27 @@ const styles = StyleSheet.create({
         padding: 12,
         width: 150,
         textAlign: "center"
+    },
+    modalContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "rgba(0,0,0,0.5)"
+    },
+    modalContent: {
+        backgroundColor: "#fff",
+        padding: 20,
+        borderRadius: 10
+    },
+    modalTitle: {
+        fontWeight: "bold",
+        fontSize: 18
+    },
+    modalText: {
+        marginTop: 10
+    },
+    modalButton: {
+        marginTop: 20
     }
 })
 
